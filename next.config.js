20e2ed79a5a22a1f150e730549ae8167c42c2514
mwatersupci2 <mwatersupci2@users.noreply.github.com>
2025-09-27 08:01:14 -0400

    Configure Next.js to allow images from Vercel blob storage domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // App Router is enabled by default in Next.js 13+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'wxgzrhzbhz6yju6a.public.blob.vercel-storage.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
